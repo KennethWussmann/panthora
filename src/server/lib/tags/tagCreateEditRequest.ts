ad76e6e5f941813069891d8394f6bda7ead04bc3
Kenneth Wußmann <kenneth@wussmann.net>
2023-11-19 15:29:51 +0100

    Fix creation of tags without parent

--- src/server/lib/tags/tagCreateEditRequest.ts
+++ src/server/lib/tags/tagCreateEditRequest.ts
@@ -4,7 +4,7 @@ export const tagCreateEditRequest = z.object({
   id: z.string().nullable().default(null),
   teamId: z.string(),
   name: z.string(),
-  parentId: z.string().nullable(),
+  parentId: z.string().nullable().default(null),
 });
 
 export type TagCreateEditRequest = z.infer<typeof tagCreateEditRequest>;
