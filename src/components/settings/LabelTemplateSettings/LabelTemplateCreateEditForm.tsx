6ab0132beeac96d1574a34d5650f126d17d2dd43
Kenneth Wußmann <kenneth@wussmann.net>
2024-02-17 12:29:00 +0100

    Fix decimal type

--- src/components/settings/LabelTemplateSettings/LabelTemplateCreateEditForm.tsx
+++ src/components/settings/LabelTemplateSettings/LabelTemplateCreateEditForm.tsx
@@ -57,7 +57,7 @@ export const LabelTemplateCreateEditForm = ({
       height: labelTemplate?.height,
       padding: labelTemplate?.padding,
       fontSize: labelTemplate?.fontSize,
-      qrCodeScale: labelTemplate?.qrCodeScale,
+      qrCodeScale: labelTemplate?.qrCodeScale?.toNumber(),
       components: labelTemplate?.components ?? [
         LabelComponents.QR_CODE,
         LabelComponents.ASSET_ID,
