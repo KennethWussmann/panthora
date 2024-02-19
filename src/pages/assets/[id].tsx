f879710b3d168f5002e340967772647182cef29a
Kenneth Wußmann <kenneth@wussmann.net>
2024-02-19 02:11:51 +0100

    Fix QR Code url and add redirect

--- src/pages/assets/[id].tsx
+++ src/pages/assets/[id].tsx
@@ -1,16 +1,27 @@
 import Error from "next/error";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { api } from "~/utils/api";
 
 export default function AssetDetails() {
-  const { query } = useRouter();
+  const { query, push } = useRouter();
   const assetId = query.id ? query.id : undefined;
+  const { data: asset, isLoading } = api.asset.get.useQuery(
+    typeof assetId === "string" && !!assetId ? assetId : "",
+    {
+      enabled: typeof assetId === "string" && !!assetId,
+    }
+  );
+
+  useEffect(() => {
+    if (asset) {
+      void push(`/assets/edit/${asset.id}`);
+    }
+  }, [asset, push]);
 
   if (typeof assetId !== "string") {
     return <Error statusCode={404} />;
   }
-
-  const { data: asset, isLoading } = api.asset.get.useQuery(assetId);
 
   if (isLoading) {
     return null;
