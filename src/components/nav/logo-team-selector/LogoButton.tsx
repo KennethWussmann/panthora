410cf948b84c86ddc2a448dc6d5a73e200028d94
Kenneth Wußmann <kenneth@wussmann.net>
2024-02-27 22:14:32 +0100

    Fix mobile views

--- src/components/nav/logo-team-selector/LogoButton.tsx
+++ src/components/nav/logo-team-selector/LogoButton.tsx
@@ -1,18 +1,24 @@
-import { Button, type ButtonProps } from "@chakra-ui/react";
+import { type ButtonProps, IconButton } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { LogoIcon } from "~/components/common/LogoIcon";
 import { LogoPanthora } from "~/components/common/LogoPanthora";
 
-export const LogoButton = (props: ButtonProps) => {
+export const LogoButton = (props: ButtonProps & { showIcon?: boolean }) => {
   const { push } = useRouter();
 
   return (
-    <Button
+    <IconButton
       variant={"ghost"}
-      {...props}
-      p={0}
+      aria-label={"Panthora"}
       onClick={() => void push("/dashboard")}
-    >
-      <LogoPanthora h={"30px"} w={"128px"} />
-    </Button>
+      icon={
+        props.showIcon ? (
+          <LogoIcon size={"30px"} />
+        ) : (
+          <LogoPanthora h={"30px"} w={"128px"} />
+        )
+      }
+      {...props}
+    />
   );
 };
