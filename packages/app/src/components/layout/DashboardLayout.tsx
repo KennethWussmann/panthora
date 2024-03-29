87f17b37c31ddc4b25c02d8d8abaee72f5eeb5ba
Kenneth Wußmann <kenneth@wussmann.net>
2024-03-29 21:38:17 +0100

    #127 Fix re-render on layout switch (#128)

--- packages/app/src/components/layout/DashboardLayout.tsx
+++ packages/app/src/components/layout/DashboardLayout.tsx
@@ -1,53 +1,36 @@
 import { type ReactNode } from "react";
-import { NavSidebar } from "../nav/NavSidebar";
-import { Container, Flex, Show, Spacer } from "@chakra-ui/react";
-import { NavTopbar } from "../nav/NavTopbar";
+import { Container, Flex, Show } from "@chakra-ui/react";
 import { Breadcrumbs } from "../breadcrumbs/Breadcrumbs";
 import { TeamInviteBanner } from "../team-invite/TeamInviteBanner";
 import { Restricted } from "../auth/Restricted";
 import { OnboardingRedirect } from "../onboarding/OnboardingRedirect";
+import { NavSidebar } from "../nav/NavSidebar";
+import { NavTopbar } from "../nav/NavTopbar";
 
-const DesktopLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <Flex minH="100vh" flexDirection="row">
-      <Flex position="sticky" top="0" maxH="100vh" zIndex="1">
-        <NavSidebar />
-      </Flex>
-      <Flex flex="1" p={6} direction="column" overflowY="scroll">
+export const DashboardLayout = ({ children }: { children: ReactNode }) => (
+  <Restricted>
+    <OnboardingRedirect />
+    <Flex minH="100vh" flexDirection={{ base: "column", md: "row" }}>
+      <Show above="md">
+        <Flex position="sticky" top="0" maxH="100vh" zIndex="1">
+          <NavSidebar />
+        </Flex>
+      </Show>
+      <Show below="md">
+        <NavTopbar />
+      </Show>
+      <Flex
+        as={Container}
+        flexDir={"column"}
+        overflowY={"scroll"}
+        maxWidth={"full"}
+        p={{ base: 4, md: 6 }}
+        mt={{ base: "70px", md: 0 }}
+      >
         <TeamInviteBanner />
         <Breadcrumbs />
-
         {children}
       </Flex>
     </Flex>
-  );
-};
-
-const MobileLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <>
-      <NavTopbar />
-      <Spacer mt={"70px"} />
-      <Container maxW={"container.lg"} py={6}>
-        <TeamInviteBanner />
-        <Breadcrumbs />
-
-        {children}
-      </Container>
-    </>
-  );
-};
-
-export const DashboardLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <Restricted>
-      <OnboardingRedirect />
-      <Show above="md">
-        <DesktopLayout>{children}</DesktopLayout>
-      </Show>
-      <Show below="md">
-        <MobileLayout>{children}</MobileLayout>
-      </Show>
-    </Restricted>
-  );
-};
+  </Restricted>
+);
